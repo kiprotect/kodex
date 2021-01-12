c7b07aa9e6757cc8c8e9ab127e607b9748ad5775
Thomas Müller <thomas.mueller@kiprotect.com>
2021-01-12 15:45:08 +0100

    Updated copyright headers.

--- cmd/commands.go
+++ cmd/commands.go
@@ -1,5 +1,5 @@
 // Kodex (Community Edition - CE) - Privacy & Security Engineering Platform
-// Copyright (C) 2020  KIProtect GmbH (HRB 208395B) - Germany
+// Copyright (C) 2019-2021  KIProtect GmbH (HRB 208395B) - Germany
 //
 // This program is free software: you can redistribute it and/or modify
 // it under the terms of the GNU Affero General Public License as
