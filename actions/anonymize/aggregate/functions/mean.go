f0ea550ab98ce77c6f477ab27048f9db9a9c9a47
Andreas Dewes <andreas.dewes@kiprotect.com>
2021-01-12 15:45:08 +0100

    Updated copyright headers.

--- actions/anonymize/aggregate/functions/mean.go
+++ actions/anonymize/aggregate/functions/mean.go
@@ -1,5 +1,5 @@
-// KIProtect (Community Edition - CE) - Privacy & Security Engineering Platform
-// Copyright (C) 2020  KIProtect GmbH (HRB 208395B) - Germany
+// Kodex (Community Edition - CE) - Privacy & Security Engineering Platform
+// Copyright (C) 2019-2021  KIProtect GmbH (HRB 208395B) - Germany
 //
 // This program is free software: you can redistribute it and/or modify
 // it under the terms of the GNU Affero General Public License as
