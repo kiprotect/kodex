a6acf108382cb71112b7f299efef4f6d2c548d87
Thomas Müller <thomas.mueller@kiprotect.com>
2023-04-28 13:59:02 +0200

    Implement diffing and add tests.

--- api/changes.go
+++ api/changes.go
@@ -10,6 +10,7 @@ const (
 	Insert Op = 0
 	Remove    = 1
 	Update    = 2
+	Swap      = 3
 )
 
 type PathType int
@@ -30,11 +31,53 @@ type PathElement struct {
 	Value      any      `json:"value"`
 }
 
+func (p PathElement) String() string {
+	switch p.PathType {
+	case Direct:
+		return fmt.Sprintf("%s", p.Name)
+	case ById:
+		return fmt.Sprintf("%s=%v", p.Identifier, p.Value)
+	case Array:
+		return fmt.Sprintf("%d", p.Index)
+	}
+	return "unknown"
+}
+
 type Change struct {
 	Op          Op            `json:"op"`
 	Value       any           `json:"value"`
 	Path        []PathElement `json:"path"`
 	Description string        `json:"description"`
+}
+
+func (c Change) String() string {
+
+	var path string
+	var op string
+
+	switch c.Op {
+	case Insert:
+		op = "insert"
+	case Remove:
+		op = "remove"
+	case Update:
+		op = "update"
+	case Swap:
+		op = "swap"
+	default:
+		op = "unknown"
+	}
+
+	for _, pathElement := range c.Path {
+
+		if path != "" {
+			path += "/"
+		}
+
+		path += pathElement.String()
+	}
+
+	return fmt.Sprintf("%s(path=%s value=%v)", op, path, c.Value)
 }
 
 func ArrayPath(index int) PathElement {
@@ -57,6 +100,279 @@ func ByIdPath(identifier string, value any) PathElement {
 		Identifier: identifier,
 		Value:      value,
 	}
+}
+
+func prependPath(changes []Change, pathElements []PathElement) []Change {
+	newChanges := make([]Change, 0, len(changes))
+
+	for _, change := range changes {
+		change.Path = append(pathElements, change.Path...)
+		newChanges = append(newChanges, change)
+	}
+
+	return newChanges
+}
+
+// Performs a diff on two maps
+func diffMap(a, b map[string]any) []Change {
+
+	changes := make([]Change, 0)
+
+	// we check if any of a's keys are missing in b
+	for ka, va := range a {
+
+		_, ok := b[ka]
+
+		if !ok {
+			// this value was removed, we use a remove operation
+			changes = append(changes, Change{Op: Remove, Value: va, Path: []PathElement{DirectPath(ka)}})
+		}
+	}
+
+	// we check if any of b's keys are missing in a
+	for kb, vb := range b {
+
+		_, ok := a[kb]
+
+		if !ok {
+			// this is a new value, we use an update operation to add it
+			changes = append(changes, Change{Op: Update, Value: vb, Path: []PathElement{DirectPath(kb)}})
+		}
+	}
+
+	// we compare all common keys in a & b
+	for ka, va := range a {
+
+		vb, ok := b[ka]
+
+		if !ok {
+			continue
+		}
+
+		// we diff the changes between a & b, prepend the current path and return them
+		changes = append(changes, prependPath(diffAny(va, vb), []PathElement{DirectPath(ka)})...)
+
+	}
+
+	return changes
+}
+
+// Performs a diff on two arrays
+func diffArray(a, b []any) []Change {
+
+	changes := make([]Change, 0)
+
+	// we check if both arrays have maps with identifiers
+
+	withIds := true
+
+	for _, va := range a {
+		if mapVa, ok := va.(map[string]any); ok {
+			if _, ok := mapVa["id"]; !ok {
+				withIds = false
+				break
+			}
+		} else {
+			withIds = false
+			break
+		}
+	}
+
+	if withIds {
+		for _, vb := range b {
+			if mapVb, ok := vb.(map[string]any); ok {
+				if _, ok := mapVb["id"]; !ok {
+					withIds = false
+					break
+				}
+			} else {
+				withIds = false
+				break
+			}
+		}
+	}
+
+	equal := func(a, b any) bool {
+		if withIds {
+			mapA := a.(map[string]any)
+			mapB := b.(map[string]any)
+			return mapA["id"] == mapB["id"]
+		}
+		return len(diffAny(a, b)) == 0
+	}
+
+	// - we find removed objects and add 'Remove' changes for them
+	// - we find added objects and add 'Insert' changes for them
+	// - after that the two lists should have identical objects
+	//   just with different order, we add 'Swap' changes to fix that
+
+	// finding removed objects
+
+	am := make([]any, 0, len(a))
+
+	usedObjects := make(map[int]bool)
+	removedObjects := 0
+
+	// we add 'Remove' changes for removed objects
+	for i, va := range a {
+		found := false
+		for j, vb := range b {
+
+			if _, ok := usedObjects[j]; ok {
+				continue
+			}
+
+			if equal(va, vb) {
+				found = true
+				usedObjects[j] = true
+				am = append(am, va)
+				break
+			}
+		}
+
+		if !found {
+
+			var path []PathElement
+
+			if withIds {
+				path = []PathElement{ByIdPath("id", va.(map[string]any)["id"])}
+			} else {
+				path = []PathElement{ArrayPath(i - removedObjects)}
+			}
+
+			// we keep track of how many objects we already removed
+			removedObjects += 1
+
+			// this object was removed, we add a corresponding change
+			changes = append(changes, Change{
+				Op:    Remove,
+				Value: va,
+				Path:  path,
+			})
+		}
+	}
+
+	usedObjects = make(map[int]bool)
+
+	// we add 'Insert' changes for new objects
+	for i, vb := range b {
+
+		found := false
+
+		for j, va := range a {
+
+			if _, ok := usedObjects[j]; ok {
+				continue
+			}
+
+			if equal(va, vb) {
+				found = true
+				usedObjects[j] = true
+				break
+			}
+		}
+
+		if !found {
+
+			// this object was added, we add the corresponding change
+			changes = append(changes, Change{
+				Op:    Insert,
+				Value: vb,
+				Path:  []PathElement{ArrayPath(i)},
+			})
+
+			if i >= len(am) {
+				am = append(am, vb)
+			} else {
+				// we insert the added object to the working list
+				am = append(am[:i+1], am[i:]...)
+				am[i] = vb
+			}
+
+		}
+	}
+
+	// now only the order of objects in the working list and b should differ
+
+	for i, vb := range b {
+
+		var j int
+
+		for j = i; j < len(am); j++ {
+			if equal(vb, am[j]) {
+				break
+			}
+		}
+
+		if i != j {
+			// element is at position i but should be at j, we swap it
+			changes = append(changes, Change{
+				Op:    Swap,
+				Value: j, // target
+				Path:  []PathElement{ArrayPath(i)},
+			})
+
+			// we swap the items in the working list
+			am[i], am[j] = am[j], am[i]
+		}
+	}
+
+	// now only values should differ
+	for i, va := range am {
+
+		var path []PathElement
+
+		if withIds {
+			path = []PathElement{ByIdPath("id", va.(map[string]any)["id"])}
+		} else {
+			path = []PathElement{ArrayPath(i)}
+		}
+
+		if elementChanges := diffAny(va, b[i]); len(elementChanges) > 0 {
+			changes = append(changes, prependPath(elementChanges, path)...)
+		}
+
+	}
+
+	return changes
+}
+
+func diffAny(a, b any) []Change {
+
+	taMap, okA := a.(map[string]any)
+	tbMap, okB := b.(map[string]any)
+
+	if okA && okB {
+		// two maps, we diff them
+		return diffMap(taMap, tbMap)
+	}
+
+	taArray, okA := a.([]any)
+	tbArray, okB := b.([]any)
+
+	if okA && okB {
+		// two array, we diff them
+		return diffArray(taArray, tbArray)
+	}
+
+	if a != b {
+		// the value changed, we return an update c hange
+		return []Change{
+			{
+				Op:    Update,
+				Value: b,
+				Path:  []PathElement{},
+			},
+		}
+	}
+
+	// nothing changed
+	return nil
+}
+
+// Returns the difference between two data structures as a sequence of changes
+func Diff(a, b map[string]any) []Change {
+	return diffMap(a, b)
 }
 
 // Applies a sequence of changes to an object
@@ -96,7 +412,7 @@ func ApplyChanges(object map[string]any, changes []Change) error {
 				if arrayObj, ok := obj.([]any); !ok {
 					return fmt.Errorf("expected a array")
 				} else if len(arrayObj) <= pathElement.Index {
-					return fmt.Errorf("out of bounds")
+					return fmt.Errorf("array element out of bounds")
 				} else {
 					obj = arrayObj
 				}
@@ -129,6 +445,44 @@ func ApplyChanges(object map[string]any, changes []Change) error {
 
 		switch change.Op {
 		// we expect an array element and a 'Array' path element
+		case Swap:
+
+			targetIndex, ok := change.Value.(int)
+
+			if !ok {
+				return fmt.Errorf("expected a target index")
+			}
+
+			if lastPathElement.PathType != Array {
+				return fmt.Errorf("expected an array path element")
+			}
+
+			if arrayObj, ok := obj.([]any); !ok {
+				return fmt.Errorf("expected an array for insertion, got %T", obj)
+			} else if lastPathElement.Index >= len(arrayObj) || lastPathElement.Index < 0 {
+				return fmt.Errorf("move: source out of bounds")
+			} else {
+
+				if targetIndex < 0 || targetIndex > len(arrayObj)-1 {
+					return fmt.Errorf("move: target out of bounds")
+				}
+
+				if len(change.Path) < 2 {
+					return fmt.Errorf("expected at least 2 path elements")
+				}
+
+				beforeLastPathElement := change.Path[len(change.Path)-2]
+
+				if beforeLastPathElement.PathType != Direct {
+					return fmt.Errorf("expected a direct path")
+				}
+
+				index := lastPathElement.Index
+
+				arrayObj[index], arrayObj[targetIndex] = arrayObj[targetIndex], arrayObj[index]
+
+			}
+		// we expect an array element and a 'Array' path element
 		case Insert:
 
 			if lastPathElement.PathType != Array {
@@ -138,7 +492,7 @@ func ApplyChanges(object map[string]any, changes []Change) error {
 			if arrayObj, ok := obj.([]any); !ok {
 				return fmt.Errorf("expected an array for insertion, got %T", obj)
 			} else if lastPathElement.Index > len(arrayObj) || lastPathElement.Index < -1 {
-				return fmt.Errorf("out of bounds")
+				return fmt.Errorf("insert: out of bounds")
 			} else {
 
 				// the parent object should be a map
@@ -195,7 +549,7 @@ func ApplyChanges(object map[string]any, changes []Change) error {
 
 				if lastPathElement.PathType == Array {
 					if lastPathElement.Index >= len(arrayObj) || lastPathElement.Index < -1 {
-						return fmt.Errorf("out of bounds")
+						return fmt.Errorf("remove: out of bounds")
 					}
 					index = lastPathElement.Index
 				} else if lastPathElement.PathType == ById {
--- api/changes_test.go
+++ api/changes_test.go
@@ -9,6 +9,130 @@ type TestCase struct {
 	Expected []any
 }
 
+func TestMapDiff(t *testing.T) {
+	a := map[string]any{
+		"foo": map[string]any{"bum": 1},
+		"baz": []any{1, 2, 3, "test"},
+	}
+
+	b := map[string]any{
+		"foo": map[string]any{"bum": 2},
+		"baz": []any{"test", 1, 2, 3, "foo"},
+		"number": 4,
+	}
+
+	changes := Diff(a, b)
+
+	if len(changes) != 6 {
+		t.Fatalf("expected 5 changes, got %d - %v", len(changes), changes)
+	}
+
+	if err := ApplyChanges(a, changes); err != nil {
+		t.Fatal(err)
+	}
+
+	if newChanges := Diff(a, b); len(newChanges) != 0 {
+		t.Fatalf("should be identical: %v - %v - %v", a, b, changes)
+	}
+}
+
+func TestDiffWithIds(t *testing.T) {
+	a := map[string]any{
+		"foo": []any{map[string]any{"id": "a"}, map[string]any{"id": "b"}, map[string]any{"id": "c"}},
+	}
+
+	b := map[string]any{
+		"foo": []any{map[string]any{"id": "a", "baz": "bam"}, map[string]any{"id": "c"}, map[string]any{"id": "d"}},
+	}
+
+	changes := Diff(a, b)
+
+	if len(changes) != 3 {
+		t.Fatalf("expected 3 changes, got %d - %v", len(changes), changes)
+	}
+
+	if err := ApplyChanges(a, changes); err != nil {
+		t.Fatal(err)
+	}
+
+	changes = Diff(a, b)
+
+	if len(changes) != 0 {
+		t.Fatalf("should be identical - %v vs %v", a, b)
+	}
+
+}
+
+func TestDiffWithoutIds(t *testing.T) {
+	a := map[string]any{
+		"foo": []any{1, map[string]any{"blub": "blab"}, map[string]any{"blip": "blop"}, "aka", 5, 6},
+	}
+
+	b := map[string]any{
+		"foo": []any{"aka", "aka", map[string]any{"blub": "blab"}, map[string]any{"foo": "bar"}, 4, "mama"},
+	}
+
+	changes := Diff(a, b)
+
+	if len(changes) != 10 {
+		t.Fatalf("expected 10 changes, got %d - %v", len(changes), changes)
+	}
+
+	if err := ApplyChanges(a, changes); err != nil {
+		t.Fatalf("%v, %v", err, changes)
+	}
+
+	newChanges := Diff(a, b)
+
+	if len(newChanges) != 0 {
+		t.Fatalf("should be identical - %v vs %v . %v", a, b, changes)
+	}
+
+}
+
+func TestSwap(t *testing.T) {
+
+	for i := 0; i < 3; i++ {
+		for j := 0; j < 3; j++ {
+
+			l := []any{map[string]any{"id": "a"}, map[string]any{"id": "b"}, map[string]any{"id": "c"}}
+
+			data := map[string]any{
+				"foo": append([]any{}, l...),
+			}
+
+			changes := []Change{
+				{
+					Op:    Swap,
+					Value: j, // target index
+					Path: []PathElement{
+						{
+							PathType: Direct,
+							Name:     "foo",
+						},
+						{
+							PathType: Array,
+							Index:    i, // source index
+						},
+					},
+				},
+			}
+
+			if err := ApplyChanges(data, changes); err != nil {
+				t.Fatal(err)
+			}
+
+			ll := data["foo"].([]any)
+
+			if ll[j].(map[string]any)["id"] != l[i].(map[string]any)["id"] {
+				t.Fatalf("unexpected: %d - %d - %v - %v", i, j, ll, l)
+			}
+
+		}
+	}
+
+}
+
 func TestRemoveMapById(t *testing.T) {
 
 	data := map[string]any{
