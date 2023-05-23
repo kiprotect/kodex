5f491c44391a127bdffb18783cb3ea60371eaa52
Thomas Müller <thomas.mueller@kiprotect.com>
2023-05-23 09:36:42 +0200

    Work on navbar.

--- web/navbar.go
+++ web/navbar.go
@@ -36,7 +36,66 @@ func Navbar(c Context) Element {
 			),
 		),
 		Div(
-			H1(Class("bulma-navbar-item", "bulma-navbar-title"), user.Email),
+			Class("bulma-navbar-meanu"),
+			Div(
+				Class("bulma-navbar-end"),
+				Div(
+					Class("bulma-navbar-dropdown-menu", "bulma-navbar-item", "bulma-has-dropdown"),
+				),
+				Div(
+					Id("user-dropdown"),
+					Class("bulma-navbar-dropdown-menu", "bulma-navbar-item", "bulma-has-dropdown"),
+					A(
+						Attrib("aria-has-popup")("true"),
+						Attrib("aria-expanded")("true"),
+						Class("bulma-navbar-link"),
+						OnClick("toggleUserMenu(event)"),
+						Div(
+							Class("kip-nowrap"),
+							Span(
+								Class("icon", "is-small"),
+								I(
+									Class("fas", "fa-user-circle"),
+								),
+							),
+							Span(
+								Class("kip-overflow-ellipsis", "bulma-is-hidden-navbar"),
+								user.Email,
+							),
+						),
+					),
+					Div(
+						Class("kip-navbar-dropdown", "bulma-navbar-dropdown", "bulma-is-right"),
+						A(
+							Class("kip-navbar-dropdown__item", "bulma-dropdown-item"),
+							Href("/logout"),
+							Span(
+								Class("icon", "is-small"),
+								I(
+									Class("fas", "fa-sign-out-alt"),
+								),
+							),
+							"Logout",
+						),
+					),
+					Script(`
+
+let dropdown = document.getElementById('user-dropdown');
+
+function closeMenu(e){
+	if (!dropdown.contains(e.target)){
+		dropdown.classList.remove('bulma-is-active');
+	}
+}
+
+window.addEventListener("click", closeMenu, false);
+
+function toggleUserMenu(e){
+	dropdown.classList.toggle('bulma-is-active');
+}
+					`),
+				),
+			),
 		),
 	)
 }
